78f8e1dc6ac99ded284de19e907ac0b49f02a75a
LucasLemosR <105325775+LucasLemosR@users.noreply.github.com>
2022-11-23 18:29:44 -0300

    Add files via upload

---
+++ clientes.js
@@ -0,0 +1,22 @@
+const pessoas = [
+    { name: "Fabiana Araújo", age: 33 },
+    { name: "Gabriel Gomes", age: 25 },
+    { name: "Fernando Henrique", age: 17 },
+    { name: "Ana Luiza", age: 2 },
+    { name: "Geralda do Nascimento", age: 93 },
+    { name: "Miguel Souza", age: 70 },
+    { name: "Antonio Miguel", age: 69 },
+];
+
+function obterMediaIdades(pessoas) {
+   
+    const idades = pessoas.map(function (pessoa) {
+
+    })
+   
+    const soma = pessoas.reduce(function (total, idade) {
+        
+    }, 0)
+   
+    return (soma/idades)
+   }
